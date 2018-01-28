89886242dde8447e363f754fda66bd4ccc329370
uaginadiver <fc2006@mail.ru>
2018-01-29 01:34:51 +0300

    1

---
+++ js/buttons.js
@@ -0,0 +1,109 @@
+
+$(document).ready(function() {
+
+    (function ($) {
+
+
+        $('#btn0').click(function (h) {
+$('#btn1').show();
+$('#login').show();
+$('#btn0').hide();
+$('.loginform').hide();
+}
+
+
+
+
+
+
+
+      );
+    })(jQuery);
+
+});
+
+
+$(document).ready(function() {
+
+    (function ($) {
+
+
+        $('#btn1').click(function (g) {
+$('#loginwindow').hide();
+$('#payment_method').show();
+}
+
+
+
+
+
+
+
+      );
+    })(jQuery);
+
+});
+
+$(document).ready(function() {
+
+    (function ($) {
+
+
+        $('.pmlicon').click(function (h) {
+$('#payment_method').hide();
+$('#merchant').show();
+}
+
+
+
+
+
+
+
+      );
+    })(jQuery);
+
+});
+
+$(document).ready(function() {
+
+    (function ($) {
+
+
+        $('#btn3').click(function (h) {
+$('#merchant').hide();
+$('#congratilations').show();
+}
+
+
+
+
+
+
+
+      );
+    })(jQuery);
+
+});
+
+
+$(document).ready(function() {
+
+    (function ($) {
+
+
+        $('#btn4').click(function (h) {
+$('#congratilations').hide();
+$('#loginwindow').show();
+}
+
+
+
+
+
+
+
+      );
+    })(jQuery);
+
+});
